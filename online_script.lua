efbd3591b0d9e8e2e01f2821e3186c2850e1b795
SimpleCodeStudio <107055683+SimpleCodeStudio@users.noreply.github.com>
2022-11-21 02:35:59 +0300

    Update online_script.lua

--- online_script.lua
+++ online_script.lua
@@ -2,3 +2,6 @@
 gg.alert("Online Script Loaded!")
 
 m = gg.choice({"Read Message"})
+if m == 1 then
+  gg.alert("I hope this file helps you. If it works I will happy, you can contact me if it is not understand. And remember. Previous script is closed. Lol")
+end
